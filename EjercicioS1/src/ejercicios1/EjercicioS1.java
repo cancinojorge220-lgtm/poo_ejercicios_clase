dd08a763a40a829849972a3c855e8fbac2d358a6
jorge-ted <cancinojorge220@gmail.com>
2026-04-05 16:05:47 -0500

    Ejercicio POO Semana 2

---
+++ EjercicioS1/src/ejercicios1/EjercicioS1.java
@@ -0,0 +1,8 @@
+package ejercicios1;
+
+public class EjercicioS1 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ EjercicioS1/src/ejercicios1/Producto.java
@@ -0,0 +1,9 @@
+package ejercicios1;
+
+public class Producto {
+    private int idProducto;
+    private String nombre;
+    private double precio;
+    private int stock;
+    
+}
