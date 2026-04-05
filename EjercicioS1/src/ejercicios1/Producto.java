b9379253e66a35532a8cf176e633f91849f95a83
jorge-ted <cancinojorge220@gmail.com>
2026-04-05 17:13:39 -0500

    Agregado de SET

--- EjercicioS1/src/ejercicios1/Producto.java
+++ EjercicioS1/src/ejercicios1/Producto.java
@@ -12,5 +12,38 @@ public class Producto {
         this.precio = precio;
         this.stock = stock;
     }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    
     
 }
